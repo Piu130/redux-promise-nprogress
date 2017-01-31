aa9828ab166ab39ab714cfd414c736c4512bbc81
Ramon Boss <ramon.boss@outlook.com>
2017-01-31 23:25:10 +0100

    reverted previous change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import NProgress from 'nprogress'
 
-export default function () {
-  return store => next => action => {
-    if(action.type) {
-      if(action.type.endsWith('PENDING')) {
-        NProgress.start()
-      } else if (action.type.endsWith('FULFILLED') || action.type.endsWith('REJECTED')) {
-        NProgress.done()
-      }
+export default store => next => action => {
+  if(action.type) {
+    if(action.type.endsWith('PENDING')) {
+      NProgress.start()
+    } else if (action.type.endsWith('FULFILLED') || action.type.endsWith('REJECTED')) {
+      NProgress.done()
     }
-    return next(action)
   }
+  return next(action)
 }
